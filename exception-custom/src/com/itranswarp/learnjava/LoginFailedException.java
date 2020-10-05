328dc554ba6f830e4f5f074fb6338d37dc4ffe89
chaos2171053 <chaos2171053@gmail.com>
2020-10-05 14:37:57 +0800

    excetion-custom

---
+++ exception-custom/src/com/itranswarp/learnjava/LoginFailedException.java
@@ -0,0 +1,12 @@
+package com.itranswarp.learnjava;
+
+public class LoginFailedException extends Exception {
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+
+	public LoginFailedException(String message) {
+		super(message);
+	}
+}
