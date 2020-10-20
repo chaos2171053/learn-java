8a0d7bbb7cbe3a49c6a4970f8a13bfbf033f7de2
qinliangchao <qinliangchao@timework.cn>
2020-10-20 17:56:22 +0800

    ADD:tomcat

---
+++ web-servlet-hello/src/main/java/com/itranswarp/learnjava/servlet/HelloServlet.java
@@ -0,0 +1,27 @@
+package com.itranswarp.learnjava.servlet;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@WebServlet(urlPatterns = "/")
+public class HelloServlet extends HttpServlet {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+
+	@Override
+	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+		resp.setContentType("text/html");
+		PrintWriter pw = resp.getWriter();
+		pw.write("<h1>Hello, 666!</h1>");
+		pw.flush();
+	}
+}
