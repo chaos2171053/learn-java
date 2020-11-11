a88a95048a7ead32a2bc34de670ccab89655bc82
chaos2171053 <chaos2171053@gmail.com>
2020-11-11 21:51:36 +0800

    [ADD]bean prototype

---
+++ src/com/chaos/spring/demo/BeanScopeDemoApp.java
@@ -0,0 +1,29 @@
+package com.chaos.spring.demo;
+
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+public class BeanScopeDemoApp {
+
+	public static void main(String[] args) {
+		// load the spring configuration file
+		ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext("beanScope-applicationContext.xml");
+
+		// retrieve bean from spring container
+		Coach theCoach = context.getBean("myCoach", Coach.class);
+
+		Coach alCoach = context.getBean("myCoach", Coach.class);
+
+		// check if they are the sam
+		Boolean result = (theCoach == alCoach);
+
+		System.out.println("\nPointing to the same object " + result);
+
+		System.out.println("\nMemory location for theCoach " + theCoach);
+
+		System.out.println("\nMemory location for alCoach " + alCoach);
+
+		context.close();
+
+	}
+
+}
