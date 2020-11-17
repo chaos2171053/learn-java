2148dc71fe8e85db66ac43c096363990b3088589
chaos2171053 <chaos2171053@gmail.com>
2020-11-17 21:56:42 +0800

    [ADD]spring config

---
+++ spring-demo-annotations/src/com/chaos/springdemo/SportConfig.java
@@ -0,0 +1,10 @@
+package com.chaos.springdemo;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@ComponentScan("com.chaos.springdemo")
+public class SportConfig {
+
+}
