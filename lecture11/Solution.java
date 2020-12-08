a8bfad7c9fb127a0669ff964a00c1f98a1e6579d
qinliangchao <qinliangchao@timework.cn>
2020-12-08 14:09:41 +0800

    [ADD]lecture 11

--- lecture11/Solution.java
+++ lecture11/Solution.java
@@ -1,35 +1,74 @@
-import java.util.HashMap;
-import java.util.Map;
+import java.util.ArrayList;
+import java.util.Collections;
 
 /* 
-共享的姓氏和名字
+构建家庭
 */
 
 public class Solution {
     public static void main(String[] args) {
-        Map<String, String> map = createPeopleMap();
-        printPeopleMap(map);
+        Human c1 = new Human("C1", true, 15);
+        Human c2 = new Human("C2", false, 10);
+        Human c3 = new Human("C3", true, 5);
+
+        Human f = new Human("F", true, 33, c1, c2, c3);
+        Human m = new Human("M", false, 29, c1, c2, c3);
+
+        Human grandF1 = new Human("GF1", true, 69, f);
+        Human grandM1 = new Human("GM1", false, 65, f);
+
+        Human grandF2 = new Human("GF2", true, 63, m);
+        Human grandM2 = new Human("GM2", false, 62, m);
+
+        System.out.println(grandF1);
+        System.out.println(grandM1);
+        System.out.println(grandF2);
+        System.out.println(grandM2);
+        System.out.println(f);
+        System.out.println(m);
+        System.out.println(c1);
+        System.out.println(c2);
+        System.out.println(c3);
     }
 
-    public static Map<String, String> createPeopleMap() {
-        // 在此编写你的代码
-        Map<String, String> people = new HashMap<>();
-        people.put("Xiao7", "Ming");
-        people.put("Xiao6", "Hong1");
-        people.put("Xiao1", "Hong2");
-        people.put("Xiao2", "Hong3");
-        people.put("Xiao3", "Hong4");
-        people.put("Xiao3", "Hong5");
-        people.put("Xiao5", "Hong");
-        people.put("Hong1", "Ming");
-        people.put("Hong2", "Ming");
-        people.put("Xiao66", "Ming6");
-        return people;
-    }
+    public static class Human {
+        String name;
+        boolean sex;
+        int age;
 
-    public static void printPeopleMap(Map<String, String> map) {
-        for (Map.Entry<String, String> s : map.entrySet()) {
-            System.out.println(s.getKey() + " " + s.getValue());
+        ArrayList<Human> children = new ArrayList<>();
+
+        public Human(String name, boolean sex, int age) {
+            this.name = name;
+            this.sex = sex;
+            this.age = age;
+        }
+
+        public Human(String name, boolean sex, int age, Human... children) {
+            this.name = name;
+            this.sex = sex;
+            this.age = age;
+
+            Collections.addAll(this.children, children);
+        }
+
+        public String toString() {
+            String text = "";
+            text += "名字：" + this.name;
+            text += "，性别：" + (this.sex ? "男" : "女");
+            text += "，年龄：" + this.age;
+
+            int childCount = this.children.size();
+            if (childCount > 0) {
+                text += "，孩子：" + this.children.get(0).name;
+
+                for (int i = 1; i < childCount; i++) {
+                    Human child = this.children.get(i);
+                    text += "，" + child.name;
+                }
+            }
+            return text;
         }
     }
+
 }
