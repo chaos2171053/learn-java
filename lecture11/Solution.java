8d5350d857b0b29cab76692e53e2e760730a5f1a
qinliangchao <qinliangchao@timework.cn>
2020-12-09 14:03:27 +0800

    [ADD]transform data

--- lecture11/Solution.java
+++ lecture11/Solution.java
@@ -1,35 +1,8 @@
 public class Solution {
     public static void main(String[] args) {
-        try {
-            handleExceptions(new Solution());
-        } catch (Exception e) {
-            printStack(e);
-        }
-
-    }
-
-    public static void handleExceptions(Solution obj) {
-        obj.method1();
-        obj.method2();
-        obj.method3();
-    }
-
-    public static void printStack(Throwable throwable) {
-        System.out.println(throwable);
-        for (StackTraceElement element : throwable.getStackTrace()) {
-            System.out.println(element);
-        }
-    }
-
-    public void method1() {
-        throw new NullPointerException();
-    }
-
-    public void method2() {
-        throw new IndexOutOfBoundsException();
-    }
-
-    public void method3() {
-        throw new NumberFormatException();
+        short number = 9;
+        char zero = '0';
+        int nine = (zero + number);
+        System.out.println((char) nine);
     }
 }
