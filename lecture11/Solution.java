01e72b1e92ca17856cbc87f29d2d7d39fcf3428f
qinliangchao <qinliangchao@timework.cn>
2020-12-09 15:54:29 +0800

    [ADD]format data

--- lecture11/Solution.java
+++ lecture11/Solution.java
@@ -1,8 +1,14 @@
+/* 
+正确答案：d = 5.5
+*/
+
 public class Solution {
     public static void main(String[] args) {
-        short number = 9;
-        char zero = '0';
-        int nine = (zero + number);
-        System.out.println((char) nine);
+        int a = 5;
+        int b = 4;
+        int c = 3;
+        int e = 2;
+        double d = a + b / c / e;
+        System.out.println(d);
     }
 }
