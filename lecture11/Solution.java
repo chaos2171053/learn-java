966bb57be6626df075bf862c9305f0aa17a02f44
qinliangchao <qinliangchao@timework.cn>
2020-12-10 17:14:05 +0800

    [ADD]inheritance

--- lecture11/Solution.java
+++ lecture11/Solution.java
@@ -1,34 +1,16 @@
-import java.util.ArrayList;
-
 /* 
-字符串列表数组
+Horse 和 Pegasus
 */
 
 public class Solution {
     public static void main(String[] args) {
-        ArrayList<String>[] arrayOfStringList = createList();
-        printList(arrayOfStringList);
     }
 
-    public static ArrayList<String>[] createList() {
-        ArrayList<String>[] result = (ArrayList<String>[]) new ArrayList[3];
+    public class Horse {
 
-        result[0] = new ArrayList<String>();
-        result[1] = new ArrayList<String>();
-        result[2] = new ArrayList<String>();
-
-        result[0].add("Mama");
-        result[1].add("Mila");
-        result[2].add("Ramu");
-
-        return result;
     }
 
-    public static void printList(ArrayList<String>[] arrayOfStringList) {
-        for (ArrayList<String> list : arrayOfStringList) {
-            for (String s : list) {
-                System.out.println(s);
-            }
-        }
+    public class Pegasus extends Horse {
+
     }
 }
