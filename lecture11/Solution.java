b0822c890c9da99e4c2df0c8eff2589ed34e92d1
qinliangchao <qinliangchao@timework.cn>
2020-12-11 17:32:42 +0800

    [ADD]多态

--- lecture11/Solution.java
+++ lecture11/Solution.java
@@ -1,27 +1,33 @@
-// •	程序应在屏幕上显示文本。
-// •	Whale 类必须继承 Cow 类。
-// •	Whale 类必须重写 getName() 方法。
-// •	main() 方法必须调用 Cow 对象的 getName() 方法。
-// •	重写 Whale 类中的 getName 方法，从而使程序显示：“我不是奶牛。我是一条鲸鱼。”
-// •	main() 方法应显示调用 getName() 方法的结果。
-
+// •	程序必须在屏幕上显示文本。
+// •	不要更改 Cat 类。
+// •	不要更改 Dog 类。
+// •	不要更改 Bird 类。
+// •	不要更改 Lamp 类。
+// •	printObjectType() 方法必须显示以下消息之一，具体取决于传递给它的对象。例如，对于 Solution.Lamp 对象，它应该显示“灯”。
 public class Solution {
 
     public static void main(String[] args) {
-        Cow cow = new Whale();
-
-        System.out.println(cow.getName());
+        printObjectType(new Cat());
+        printObjectType(new Bird());
+        printObjectType(new Lamp());
+        printObjectType(new Cat());
+        printObjectType(new Dog());
     }
 
-    public static class Cow {
-        public String getName() {
-            return "我是一头牛";
-        }
+    public static void printObjectType(Object o) {
+        // 在此编写你的代码
+
     }
 
-    public static class Whale extends Cow {
-        public String getName() {
-            return "我不是奶牛。我是一条鲸鱼。";
-        }
+    public static class Cat {
+    }
+
+    public static class Dog {
+    }
+
+    public static class Bird {
+    }
+
+    public static class Lamp {
     }
 }
