6338dd215f361cd8259df430456fe89f470f053e
qinliangchao <qinliangchao@timework.cn>
2020-12-15 15:05:40 +0800

    [ADD]抽象类

--- lecture11/Solution.java
+++ lecture11/Solution.java
@@ -1,33 +1,18 @@
-// •	程序必须在屏幕上显示文本。
-// •	不要更改 Cat 类。
-// •	不要更改 Dog 类。
-// •	不要更改 Bird 类。
-// •	不要更改 Lamp 类。
-// •	printObjectType() 方法必须显示以下消息之一，具体取决于传递给它的对象。例如，对于 Solution.Lamp 对象，它应该显示“灯”。
+/* 
+•	程序不应该在屏幕上显示文本。
+•	Pet 类必须为 static。
+•	Pet 类必须为 abstract。
+•	Pet 类中只能包含一个名为 getName() 的方法。
+*/
+
 public class Solution {
-
     public static void main(String[] args) {
-        printObjectType(new Cat());
-        printObjectType(new Bird());
-        printObjectType(new Lamp());
-        printObjectType(new Cat());
-        printObjectType(new Dog());
-    }
-
-    public static void printObjectType(Object o) {
-        // 在此编写你的代码
 
     }
 
-    public static class Cat {
-    }
-
-    public static class Dog {
-    }
-
-    public static class Bird {
-    }
-
-    public static class Lamp {
+    public static abstract class Pet {
+        public String getName() {
+            return "我是一只小猫";
+        }
     }
 }
