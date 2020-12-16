39dd10a7ca874a11b8950e1a21e1ba21530ce598
qinliangchao <qinliangchao@timework.cn>
2020-12-16 17:01:05 +0800

    [ADD]interface

--- lecture11/Solution.java
+++ lecture11/Solution.java
@@ -1,9 +1,11 @@
 /* 
-•	Pet 类必须为 abstract。
-•	Dog 类不能为 abstract。
-•	Cat 类不能为 abstract。
-•	Dog 类必须实现 Pet 类的 abstract 方法。
-•	Cat 类必须实现 Pet 类的 abstract 方法。
+	Solution 类必须包含只有一个 fly() 方法的 CanFly 接口。
+•	Solution 类必须包含只有一个 move() 方法的 CanMove 接口。
+•	Solution 类必须包含只有一个 eat() 方法的 CanEat 接口。
+•	狗必须能够四处走动并进食。
+•	汽车必须能够移动。
+•	飞机必须能够移动和飞行。
+•	鸭子应该能够移动、飞行和进食。
 */
 
 public class Solution {
@@ -11,37 +13,75 @@ public class Solution {
 
     }
 
-    public static abstract class Pet {
-        public abstract String getName();
-
-        public abstract Pet getChild();
+    public interface CanFly {
+        public void fly();
     }
 
-    public static class Cat extends Pet {
-
-        @Override
-        public String getName() {
-            return null;
-        }
-
-        @Override
-        public Solution.Pet getChild() {
-            return null;
-        }
-
+    public interface CanMove {
+        public void move();
     }
 
-    public static class Dog extends Pet {
+    public interface CanEat {
+        public void eat();
+    }
+
+    public class Dog implements CanMove, CanEat {
 
         @Override
-        public String getName() {
-            return null;
+        public void eat() {
+            // TODO Auto-generated method stub
+
         }
 
         @Override
-        public Solution.Pet getChild() {
+        public void move() {
+            // TODO Auto-generated method stub
 
-            return null;
         }
+    }
+
+    public class Duck implements CanMove, CanFly, CanEat
+
+		@Override
+		public void eat() {
+			// TODO Auto-generated method stub
+			
+		}
+
+		@Override
+		public void fly() {
+			// TODO Auto-generated method stub
+			
+		}
+
+		@Override
+		public void move() {
+			// TODO Auto-generated method stub
+			
+		} {
+    }
+
+    public class Car implements CanMove {
+
+		@Override
+		public void move() {
+			// TODO Auto-generated method stub
+			
+		}
+    }
+
+    public class Airplane implements CanMove,CanFly {
+
+		@Override
+		public void fly() {
+			// TODO Auto-generated method stub
+			
+		}
+
+		@Override
+		public void move() {
+			// TODO Auto-generated method stub
+			
+		}
     }
 }
